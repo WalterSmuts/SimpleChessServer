5a8070902c0ac6267d9f8751ada8f84f6835adf0
Walter Smuts <smuts.walter@gmail.com>
2019-05-29 08:06:52 +0200

    Implemented clean path checking

--- src/ws/chess/core/Board.java
+++ src/ws/chess/core/Board.java
@@ -32,8 +32,29 @@ public class Board {
     }
 
     boolean hasCleanPath(Move move) {
-        // TODO
+        // There has to be a simpler way to check this!!!
+        int ax = move.getOriginal().getX();
+        int ay = move.getOriginal().getY();
+        int bx = move.getDestination().getX();
+        int by = move.getDestination().getY();
+
+        ax = stepTowards(ax, bx);
+        ay = stepTowards(ay, by);
+        bx = stepTowards(bx, ax);
+        by = stepTowards(by, ay);
+
+        while (ax != bx && ay != by) {
+            if (!(board[ax][ay] == null)) return false;
+            ax = stepTowards(ax, bx);
+            ay = stepTowards(ay, by);
+        }
         return true;
+    }
+
+    static int stepTowards(int a, int b) {
+        a += (a > b) ? -1 : 1;
+        if (a == b) a--;
+        return a;
     }
 
     boolean causesCheck(Move move) {
